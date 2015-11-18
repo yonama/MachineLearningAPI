524e65a7c1947e7e875025ce9f21a3f5f09064e1
黒澤 預生 <yonamachan3@gmail.com>
2015-11-19 00:23:42 +0900

    MERYの結果を使って学習 記事データを投げるとタグがリストで帰ってくるAPIを実装

--- controllers/automatic_tag.py
+++ controllers/automatic_tag.py
@@ -4,18 +4,23 @@ sys.path.append("./controllers/")
 from controller import controller
 sys.path.append("./models/")
 from articles import articles
+sys.path.append("./components/")
+from autotagcompo import autotagcompo
+from nominalization import nominalization
 
 class automatic_tag(controller):
     def setup(self):
         self.POST = self.filter.befour_filter()(self.POST)
         self.articles = articles()
+        self.tag_compo = autotagcompo()
+        self.noun_compo = nominalization()
 
     def befour_action(self):
         print("hogehoge")
 
     def POST(self, article_json):
-        #article_dic = json.loads(article_json)
-        result = self.articles.find(1000)
+        article_noun = self.noun_compo.nominalization(article_json)
+        result = self.tag_compo.make_tags_with_new_text(article_noun)
         return json.dumps(result, ensure_ascii=False)
 
     def PUT(self, article_json):
--- controllers/controller.py
+++ controllers/controller.py
@@ -1,29 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from abc import ABCMeta, abstractmethod
-import sys
-sys.path.append("./controllers/filters/")
-from filters.filter import filter
+from actor import actor
+from filters.controller_filter import controller_filter as filter
 
 #抽象化してデコレータを定義だけしとく
-class controller(object):
-    __metaclass__ = ABCMeta
+class controller(actor):
     exposed = True
-    def __init__(self):
-        self.filter = filter(self)
-        self.setup()
 
     def setup(self):
-        pass
-
-    @abstractmethod
-    def befour_action(self):
-        raise NotImplementedError()
-
-    @abstractmethod
-    def after_action(self):
-        raise NotImplementedError()
-
-    @abstractmethod
-    def around_action(self):
-        raise NotImplementedError()
+        self.filter = filter(self)
