411c901ab618e2b96a2cd795794649e2f57aa74f
黒澤 預生 <yonamachan3@gmail.com>
2015-11-25 02:38:21 +0900

    config変更

--- index.py
+++ index.py
@@ -5,6 +5,8 @@ import json
 from controllers.automatic_tag import automatic_tag as auto_tag
 
 if __name__ == '__main__':
+    cherrypy.config.update({'server.socket_host': '0.0.0.0'
+                       })
     cherrypy.tree.mount(
         auto_tag(), '/api/auto_tag',
         {'/':
